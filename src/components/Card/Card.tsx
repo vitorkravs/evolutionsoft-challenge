b402dafbd4bf553eaf6695d807cc14009e881777
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-16 17:47:34 -0300

    feat: criando a estrutura inicial do componente Card

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+
+const Card = () => {
+  const foodList = [
+    {
+      id: 1,
+      price: 12.99,
+      title: "X-salada",
+      description:
+        "Uma harmonia de sabores clássicos, com hambúrguer suculento, alface crocante, tomate fresco e queijo derretido, envolvidos por um pão macio.",
+      food_img: "/x-salada-classico.jpg",
+    },
+    {
+      id: 2,
+      price: 22.5,
+      title: "X-bacon",
+      description:
+        "Para os amantes de bacon, um hambúrguer perfeitamente grelhado, coberto com fatias crocantes de bacon, queijo, alface e tomate, uma explosão de sabor.",
+      food_img: "/x-bacon.png",
+    },
+    {
+      id: 3,
+      price: 18.99,
+      title: "X-frango",
+      description:
+        "Delicioso frango grelhado, combinado com queijo derretido, alface fresca e maionese especial, criando um lanche leve e irresistível.",
+      food_img: "/x-frango.png",
+    },
+  ];
+  return (
+    <ul>
+      {foodList.map((food) => (
+        <li key={food.id}>
+          <h3>{food.title}</h3>
+          <Image
+            src={food.food_img}
+            alt={food.food_img}
+            width={100}
+            height={100}
+          />
+          <p>{food.description}</p>
+          <p>{food.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Card;
