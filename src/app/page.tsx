a38a846ee18405bdf4229e1d8e9f9de821c3cf9d
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-16 16:31:35 -0300

    feat: adicionando componente Header na page, e enviando uma props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header/Header";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Header message="Bem Vindo ao Sistema de Contagem" />
+    </>
+  );
 }
