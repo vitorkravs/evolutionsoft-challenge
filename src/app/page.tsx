0622444dbbfcee8a11b9de08df1e6cecbd0aa8f3
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-16 17:46:12 -0300

    feat: adicionando componente Card na page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Card from "@/components/Card/Card";
 import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header message="Bem Vindo ao Sistema de Contagem" />
+      <Card />
     </>
   );
 }
