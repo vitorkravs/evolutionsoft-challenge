f5478b6357e45f82806b7629f806d17312f035c1
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-17 17:45:49 -0300

    feat: utilizando swiper para colocar dados da api no slide

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { GetServerSideProps } from "next";
-import { getServerSideProps } from "next/dist/build/templates/pages";
-import Image from "next/image";
-
+import "./styles.scss";
 interface IngredienteBase {
   id: number;
   nomesIngrediente: string[];
@@ -26,25 +23,52 @@ interface PostListProps {
   ];
 }
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Navigation } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 const PostList = ({ data }: PostListProps) => {
   return (
-    <div>
-      <h2>Receitas</h2>
-      {data.map((receita) => (
-        <div key={receita.id}>
-          <h3>{receita.receita}</h3>
-          <picture>
-            <img
-              src={receita.link_imagem}
-              alt={receita.receita}
-              width={100}
-              height={100}
-            />
-          </picture>
-          <p>{receita.ingredientes}</p>
-          <p>{receita.modo_preparo}</p>
-        </div>
-      ))}
+    <div id="post-list">
+      <h2 id="title-post-list">Receitas</h2>
+      <Swiper navigation={true} modules={[Navigation]} className="my-swiper">
+        {data.map((receita) => (
+          <SwiperSlide key={receita.id} className="swiper-item">
+            <div className="post-list-items-container">
+              <h3 className="post-list-title">{receita.receita}</h3>
+              <div className="post-content-container">
+                <picture>
+                  <img
+                    src={receita.link_imagem}
+                    alt={receita.receita}
+                    width={100}
+                    height={100}
+                    className="post-list-img"
+                  />
+                </picture>
+                <p className="post-list-ingredientes">
+                  <span className="title-ingredientes-preparo">
+                    Ingredientes
+                  </span>{" "}
+                  <br />
+                  {receita.ingredientes}
+                </p>
+                <p className="post-list-preparo">
+                  <span className="title-ingredientes-preparo">
+                    Modo de preparo
+                  </span>{" "}
+                  <br />
+                  {receita.modo_preparo}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
