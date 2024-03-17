db8cfdc5631899b59a5c989413657cdd92fc3a6e
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-17 12:23:49 -0300

    feat: adicionando componente PostList na page, e pegando dados da api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import Card from "@/components/Card/Card";
 import Header from "@/components/Header/Header";
+import PostList from "@/components/PostList/PostList";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://gold-anemone-wig.cyclic.app/receitas/todas");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <>
       <Header message="Bem Vindo ao Sistema de Contagem" />
-      <Card />
+      <main>
+        <Card />
+        <PostList data={data} />
+      </main>
     </>
   );
 }
