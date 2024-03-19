44964cb4aeaa98a2ed6324e7679c2ba055c3d251
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-18 22:33:56 -0300

    feat: visibilidade do componente Cart ao clicar no carrinho

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,38 @@
+"use client";
+
+import { IoCartOutline } from "react-icons/io5";
 import Cart from "../Cart/Cart";
 
 import "./styles.scss";
+import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 interface HeaderProps {
   message: string;
 }
 
 const Header = ({ message }: HeaderProps) => {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = (e: any) => {
+    setVisible(!visible);
+  };
   return (
     <header id="header">
       <h1 id="title-header">{message}</h1>
-      <Cart />
+      <div id="icon">
+        {visible ? (
+          <IoMdClose id="icon-cart" onClick={openMenu} />
+        ) : (
+          <IoCartOutline id="icon-cart" onClick={openMenu} />
+        )}
+      </div>
+      <div
+        className="teste"
+        style={visible ? { display: "block" } : { display: "none" }}
+      >
+        <Cart />
+      </div>
     </header>
   );
 };
