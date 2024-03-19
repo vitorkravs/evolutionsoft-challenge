099c74c62a876fe8a7ac8ff568d299ec9ff7071c
vitor kravszenko <vitorkravs3@gmail.com>
2024-03-18 22:32:39 -0300

    feat: adicionando itens e valores ao Cart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -5,7 +5,20 @@ import { IoCartOutline } from "react-icons/io5";
 const Cart = () => {
   return (
     <div id="cart">
-      <IoCartOutline id="icon-cart" />
+      <div id="cart-items-container">
+        <div className="card-items">
+          <p>X-Salada</p>
+          <p>R$ 12.99</p>
+        </div>
+        <div className="card-items">
+          <p>X-Bacon</p>
+          <p>R$ 22.50</p>
+        </div>
+        <div className="card-items">
+          <p>X-frango</p>
+          <p>R$ 18.99</p>
+        </div>
+      </div>
     </div>
   );
 };
